f2b06b8787ce055dccd5a7354259512e5e7a4052
hiroaki <koushou418@gmail.com>
2021-09-12 20:50:29 +0900

    json

--- api/res.php
+++ api/res.php
@@ -1,15 +1,17 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
-if(is_uploaded_file($_FILES['up_file']['tmp_name'])) {
-    $img_name = $_FILES['up_file']['name'];
-    if(move_uploaded_file($_FILES['up_file']['tmp_name'],"./".$img_name)){
-        // echo "<img src='" . $img_name . "'>";
-        // header("Location:index.html");
-        $json = $img_name;
-        echo json_encode($json);
-    } else {
-        echo "error while saving.";
-    }
-} else {
-    echo "file not uploaded.";
-}
+$path = __DIR__ . 'uploaded-file';
+$data = json_decode(file_get_contents('php://input'), true);
+var_dump($data);
+
+// if(is_uploaded_file($FILES['tmp_name'])) {
+//     $img_name = $FILES['tmp_name']['name'];
+//     if(move_uploaded_file($FILES['tmp_name']['name'],$path. "/" . $img_name)){
+//         $json = $img_name;
+//         echo json_encode($json);
+//     } else {
+//         echo "error while saving.";
+//     }
+// } else {
+//     echo "file not uploaded.";
+// }
--- js/dom.js
+++ js/dom.js
@@ -1,22 +1,22 @@
 const submit_button = document.getElementById("submit_button");
 submit_button.addEventListener('input', () => {
     console.log('なんすか？');
-    const requestUrl = './api/res.php';
-    const uploadFile = document.getElementById('submit_button').files[0];
+    const requestUrl = '../api/res.php';
+    const uploadFile = document.getElementById('submit_button').files[0]['name'];
+    console.log(uploadFile);
     fetch(requestUrl, {
         method: 'POST',
         body: JSON.stringify(uploadFile),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+        // headers: {
+        //     'Content-Type': 'application/json'
+        // }
     })
     .then (response => {
         console.log('ok');
-        console.log(uploadFile);
         return response.json();
     })
     .then (json => {
-        console.log(json);
+        console.log(はーい);
     })
     .catch (e => {
         alert("エラー");
